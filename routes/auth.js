b20726c4e983bd6d58832b8c22cf7dc15f92dcc3
Aromal S <aromalsanthosh@protonmail.com>
2021-12-19 21:27:21 +0530

    uservalidation, check email already exist

--- routes/auth.js
+++ routes/auth.js
@@ -3,50 +3,36 @@ const router = require('express').Router();
 const User = require('../model/User');
 
 //validation
-const Joi = require('@hapi/joi');
-const { valid } = require('@hapi/joi');
-
-
-const schema = Joi.object({
-    name: Joi.string()
-        .min(3)
-        .max(255)
-        .required(),
-    'email': Joi.string()
-        .min(7)
-        .max(255)
-        .required(),
-    'password': Joi.string()
-        .min(8)
-        .max(1024)
-        .required(),
-    'contact': Joi.string()
-        .min(10)
-        .max(10)
-        .required()
-});
+const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register',async (req,res) => {
-
-    const validation = schema.validate(req.body);
+    const {error} = registerValidation(req.body);
     
-    res.send(validation);
-    
+    if(error){
+        return res.status(400).send(error.details[0].message);
+    }
+
+    //check if user already exists
+    const emailExists = await User.findOne({email: req.body.email});
+    if(emailExists){
+        return res.status(400).send('Email already exists');
+    }
 
 
-    // const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    //     contact: req.body.contact
-    // });
+    //create user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        contact: req.body.contact
+    });
 
-    // try{
-    //     const newUser = await user.save();
-    //     res.send(newUser);
-    // }catch(err){
-    //     res.status(400).send(err);
-    // }
+    try{
+        const newUser = await user.save();
+        res.send(newUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
 });
 
 
---
+++ validation.js
@@ -0,0 +1,50 @@
+const Joi = require('@hapi/joi');
+
+//register validation
+const registerValidation = (data) => {
+    const schema = Joi.object({
+        name: Joi.string()
+            .min(3)
+            .max(255)
+            .required(),
+        'email': Joi.string()
+            .min(7)
+            .max(255)
+            .required()
+            .email(),
+        'password': Joi.string()
+            .min(8)
+            .max(1024)
+            .required(),
+        'contact': Joi.string()
+            .min(10)
+            .max(10)
+            .required()
+    });
+
+    return schema.validate(data);
+    
+}
+
+//login validation
+const loginValidation = (data) => {
+    const schema = Joi.object({
+        'email': Joi.string()
+            .min(7)
+            .max(255)
+            .required()
+            .email(),
+        'password': Joi.string()
+            .min(8)
+            .max(1024)
+            .required()
+    });
+    
+    return schema.validate(data);
+}
+
+
+
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
